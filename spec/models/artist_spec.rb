06e133fe69050cef55058c03ed0a7c5d2e185e93
David Williams <davew1982@gmail.com>
2013-12-18 14:32:51 -0500

    Wrote tests for Artist model.

---
+++ spec/models/artist_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+describe Artist do
+  it { should have_valid(:name).when('David') }
+  it { should_not have_valid(:name).when(nil, '')}
+
+  
+  it { should have_valid(:phone_no).when('4133203613')}
+  it { should_not have_valid(:phone_no).when(nil, '', '413-320-3613', 'abcde', '8675309')}
+
+
+  it { should have_valid(:email).when('davew1982@gmail.com', 'helen.c.hood@gmail.com')}
+  it { should_not have_valid(:phone_no).when(nil, '', '4535235', 'd@d', 'user@mycom', 'usermy.com', 'user', '.com', 'my.com')}
+  it { should validate_uniqueness_of(:email)}
+end
