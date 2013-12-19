2e2157c5ede4529efd98d10be438342e91237d3d
David Williams <davew1982@gmail.com>
2013-12-19 10:14:48 -0500

    Created Piece model & it passes all our tests hurray

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,9 +1,13 @@
 class Piece < ActiveRecord::Base
-belongs_to :artist
-belongs_to :collection
-belongs_to :customer
+  belongs_to :artist
+  belongs_to :collection
+  belongs_to :customer
 
-validates_presence_of :name, :price_in_cents, :style
+  validates_presence_of :artist_id, :name
+  validates :price_in_cents, numericality: { greater_than_or_equal_to: 0, allow_blank: true }
 
-
+  def buy_piece(customer)
+    self.customer_id = customer.id
+    self.available = false
+  end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -3,16 +3,18 @@ require 'spec_helper'
 describe Piece do
   # let(:piece_bought) { FactoryGirl.create(:piece, customer_id)}
 
-  it { should have_valid(:artist) }
-  it { should have_valid(:name) }
+  it { should validate_presence_of(:artist_id) }
+  it { should have_valid(:name).when('Davieee') }
+  it { should_not have_valid(:name).when(nil, '')}
 
-  it { should have_valid(:price_in_cents).when(nil, 3000000, 125, 0) }
+  it { should have_valid(:price_in_cents).when('', 3000000, 125, 0) }
   it { should_not have_valid(:price_in_cents).when(-100) }
 
   it 'should not be available if it has a customer who bought it' do
-    piece = Piece.create(artist_id: 2, name: 'My Art', customer_id: 4)
-    if piece.customer_id != nil
-      expect(piece.available).to be_false
-    end
+    piece = Piece.create(artist_id: 2, name: 'My Art')
+    customer = FactoryGirl.create(:customer)
+    piece.buy_piece(customer)
+    expect(piece.available).to be_false
+    expect(piece.customer_id).to eq(customer.id)
   end
 end
